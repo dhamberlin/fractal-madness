55c360006b1aa81c326837ca45384a969c41e7e0
David H <davidhamberlin@gmail.com>
2018-02-04 22:40:47 -0500

    fix remaining refs for github pages

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, '/src/')))
+app.use(express.static(__dirname))
+// app.get('/root', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
 
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`listenting on port ${port}`))
