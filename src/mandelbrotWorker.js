d01f10fa3848c9ffc1da04ff95bd0e70686141a8
David H <davidhamberlin@gmail.com>
2018-08-11 22:31:16 -0400

    cleanup webworker

--- src/mandelbrotWorker.js
+++ src/mandelbrotWorker.js
@@ -1,26 +1,3 @@
-function compute(y,xmin,dx,columns,maxIterations) {
-   iterationCounts = [];
-   for (let i = 0; i < columns; i++) {
-      var x0 = xmin + i * dx;
-      var y0 = y;
-      var a = x0;
-      var b = y0;
-      var ct = 0;
-      while (a*a + b*b < 4.1) {
-         ct++;
-         if (ct > maxIterations) {
-            ct = -1;
-            break;
-         }
-         var newa = a*a - b*b + x0;
-         b = 2*a*b + y0;
-         a = newa;
-      }
-      iterationCounts[i] = ct;
-   }
-   return iterationCounts;
-}
-
 function checkPixel(x, y, iterations) {
   let real = x;
   let imaginary = y;
@@ -32,7 +9,7 @@ function checkPixel(x, y, iterations) {
     real = tempReal;
     imaginary = tempImaginary;
 
-    if (real * imaginary > 5) {
+    if (real * imaginary > 2) {
       const output = i / iterations * 100;
       return output;
     }
@@ -40,37 +17,28 @@ function checkPixel(x, y, iterations) {
   return 0;
 }
 
-function checkRow(row, width, height, viewSettings) {
-  const { magnification, panX, panY, iterations } = viewSettings
-  const counts = []
-
-  for (let i = 0; i < width; i++) {
-    const x = ((row - width / 2) / magnification) - panX
-    const y = ((i - height / 2) / magnification) - panY
-    counts.push(checkPixel(x, y, iterations))
-  }
-  return counts
-}
-
 function checkJob(job) {
   const {
     pixels,
-    viewSettings: {
-      magnification, panX, panY, iterations
-    },
-    width,
-    height
+    viewSettings: { iterations },
   } = job
-  const counts = []
 
-  for (let i in pixels) {
-    const { x, y, currentX, currentY } = pixels[i]
-    // const currentX = (x / magnification) - panX
-    // const currentY = (y / magnification) - panY
-    const count = checkPixel(currentX, currentY, iterations)
-    counts.push({ x, y, currentX, currentY, count})
+  // return pixels.map(({ x, y, currentX, currentY }) => ({
+  //   x,
+  //   y,
+  //   currentX,
+  //   currentY,
+  //   count: checkPixel(currentX, currentY, iterations)
+  // }))
+  // const counts = []
+
+  for (let i = 0; i < pixels.length; i++) {
+    const { currentX, currentY } = pixels[i]
+    pixels[i].count = checkPixel(currentX, currentY, iterations)
+    // counts.push({ x, y, currentX, currentY, count})
   }
-  return counts
+  // return counts
+  return pixels
 }
 
 onmessage = (msg) => {
